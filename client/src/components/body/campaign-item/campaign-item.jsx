4eb5d9f3f237e51b1c96501772a1f57b960daa9c
Vincent La <vincent.la@nokia.com>
2018-03-19 16:42:33 -0700

    animation box for likes and description

--- client/src/components/body/campaign-item/campaign-item.jsx
+++ client/src/components/body/campaign-item/campaign-item.jsx
@@ -4,10 +4,12 @@ import LikeButton from '../../like-button/like-button.jsx';
 let style = {border: '1px black solid'}
 
 const CampaignItem = ({ campaign, handleClick }) => (
-  <div style={style}>
+  <div className="campaign-item-container" style={style}>
     <img onClick={handleClick} src={campaign.imgUrl} />
-    <div className="campaign-item-title">{campaign.id}</div>
-    <LikeButton />
+    <div className="campaign-item-description">
+      <div className="campaign-item-title">{campaign.id}</div>
+      <LikeButton />
+    </div>
   </div>
 );
 

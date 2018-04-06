77b6f0fbbba6a3f93181cf04c45a846c46ffa961
Vincent La <vincent.la@nokia.com>
2018-04-06 16:17:15 -0700

    refactoring controllers to promises

--- server/api/campaigns/campaigns.controller.js
+++ server/api/campaigns/campaigns.controller.js
@@ -1,27 +1,30 @@
 import campaignsModel from './campaigns.model';
 
 export const getAllCampaigns = (req, res, next) => {
-  campaignsModel.find({}, (err, campaigns) => {
-    if (err) {
-      return console.error(err);
-    }
-    res.json({campaigns: campaigns})
-  })
-
+  campaignsModel.find({})
+  .then(campaigns => res.json({campaigns: campaigns}))
+  .catch(err => console.error(err));
 };
 
 export const addCampaign = (req, res, next) => {
-  console.log('inside of the addcampaign');
   let campaign = new campaignsModel({
     name: req.body.name,
     data: req.body.data,
     imgUrl: req.body.imgUrl
   });
-  campaign.save((err, success) => {
+  campaign.save()
+  .then(success => {
+    res.sendStatus(200)
+    next();
+  })
+  .catch(err => console.error(err)(err, success))
+};
+
+export const removeCampaign = (req, res, next) => {
+  let target = campaignsModel.find({id: req.body.id}, (err, campaign) => {
     if (err) {
       return console.error(err);
     }
-    res.sendStatus(200);
-    next();
-  });
-};
+    campaign.remove();
+  })
+}

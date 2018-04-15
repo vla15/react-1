85e834b540879051a305b3d8d9d33eba7032b33e
Vincent La <vincent.la@nokia.com>
2018-04-14 23:27:56 -0700

    working on auth routes for user

--- server/api/auth/auth.js
+++ server/api/auth/auth.js
@@ -2,6 +2,8 @@ import UserModel from '../users/user.model';
 import expressJwt from 'express-jwt';
 import jwt from 'jsonwebtoken';
 
+const checkToken = expressJwt({ secret: 'powerup' });
+
 const verifyUser = () => (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -15,7 +17,7 @@ const verifyUser = () => (req, res, next) => {
   .then(user => {
     if (!user) {
       res.status(400).send('Your username doesn\'t match any record')
-    } else if (!user.authenticate) {
+    } else if (!user.authenticate(password)) {
       res.status(401).send('Incorrect password')
     } else {
       req.user = user;
@@ -24,3 +26,40 @@ const verifyUser = () => (req, res, next) => {
   })
   .catch(err => next(err));
 }
+
+export const decodeToken = () => (req, res, next) => {
+  if (config.disableAuth) {
+    return next();
+  }
+  // make it optional to place token on query string
+  // if it is, place it on the headers where it should be
+  // so checkToken can see it. See follow the 'Bearer 034930493' format
+  // so checkToken can see it and decode it
+  if (req.query && req.query.hasOwnProperty("access_token")) {
+    req.headers.authorization = "Bearer " + req.query.access_token;
+  }
+
+  // this will call next if token is valid
+  // and send error if its not. It will attached
+  // the decoded token to req.user
+  checkToken(req, res, next);
+};
+
+export const getUser = () => (req, res, next) => {
+  UserModel.findById(req.user.id)
+    .then(user => {
+      if (!user) {
+        res.status(401).send('Unauthorized')
+      } else {
+        req.user = user;
+        next();
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+export const signToken = (id) => jwt.sign(
+  {id},
+  'powerup',
+  {expiresIn: '30d'}
+)
--- server/api/users/user.model.js
+++ server/api/users/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema(
   {
@@ -12,6 +13,24 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.methods = {
+  //overwrites the original methods object with a new object containing two methods auth and hashPW
+  //compares sync by rerunning hash with salt against it
+  authenticate(plaintTextPassword) {
+    return bcrypt.compareSync(plaintTextPassword, this.password);
+  },
+
+  hashPassword(plaintTextPassword) {
+  if (!plaintTextPassword) {
+    throw new Error('Could not save user')
+  }
+
+  //salts the password with bcrypt before hashing
+  const salt = bcrypt.genSaltSync(10)
+  return bcrypt.hashSync(plaintTextPassword, salt)
+  }
+}
 
 const UserModel = mongoose.model('User', userSchema)
 export default UserModel;
